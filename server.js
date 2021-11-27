8a67b7fd6c48cb5fef669cdc7677a27d9cf631ae
kirill.antonov <kirill.antonov@xenial.com>
2021-11-27 14:57:25 +0200

    Upgrade to use node 16.x

--- server.js
+++ server.js
@@ -6,18 +6,14 @@
  * @param {Object} params - compound/express webserver initialization params.
  * @returns CompoundJS powered express webserver
  */
-var app = module.exports = function getServerInstance(params) {
-    params = params || {};
-    // specify current dir as default root of server
-    params.root = params.root || __dirname;
-    return require('compound').createServer(params);
-};
+const express = require('express')
 
 if (!module.parent || module.parent.isApplicationLoader) {
     var port = process.env.PORT || 3000;
     var host = process.env.HOST || '0.0.0.0';
 
-    var server = app();
+    var server = express();
+    server.use(express.static('public'));
     server.listen(port, host, function () {
         console.log(
             'Compound server listening on %s:%d within %s environment',
